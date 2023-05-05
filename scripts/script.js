2dfd35a0d86a079a6732212435428593cda9801b
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-05-05 13:40:07 -0500

    created 3 functions to add, remove and count the amount of items in a list.

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,53 @@
 $(onReady)
 
+//on ready calls that ever is in this function.
 function onReady() {
-    console.log('Hellow World');
+    console.log('Hello World');
 
-
-    
+        //calls handleSubmit on submitBtn click.
+        $('#submitBtn').on('click',handleSubmit);
+        $('#listArea').on('click', '.listItem', removeItem);
 }
+
+
+function handleSubmit(event) {
+
+        //use the event arg to stop default page reload.
+        event.preventDefault();
+        let inputOne;
+        let inputTwo;
+        //grab val from the inputs
+
+       inputOne = $('#inputOne').val();
+       inputTwo = $('#inputTwo').val();
+       console.log(inputOne, inputTwo);
+
+        $('#listArea').append(`
+        <li class="listItem">
+            <button class="todoBtn">🚫</button>
+            ${inputOne} ${inputTwo}
+        </li>
+        `);
+
+        $('#inputOne').val('');
+        $('#inputTwo').val('');
+
+        countValue();
+
+
+
+}
+
+function removeItem(){
+        $(this).remove();
+        countValue();
+
+
+
+}
+function countValue(){
+    $('#countValue').empty();
+    $('#countValue').append($('.listItem').length);
+}
+
+
